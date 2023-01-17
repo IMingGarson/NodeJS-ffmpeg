d6bbda464f14bfafa64fd61289e61b0c98e87f8d
iminghsu <iminghsu@cw.com.tw>
2023-01-17 16:14:19 +0800

    fix: add swagger endpoint

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const api = require('./src/Routes/api.js');
+const videoApi = require('./src/Routes/video/api.js');
 const app = express();
 const port = process.env.PORT || 3000;
-
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
 const corsOptions = {
     origin: [
         'http://localhost:3000',
@@ -13,11 +14,23 @@ const corsOptions = {
     methods: 'GET,POST,OPTIONS',
     allowedHeaders: ['Content-Type', 'Authorization'],
 };
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Hello World',
+            version: '1.0.0',
+        },
+    },
+    apis: ['./src/Routes/*/api.js'],
+};
+const openapiSpecification = swaggerJsdoc(swaggerOptions);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ type: 'application/json' }));
 app.use(cors(corsOptions));
-app.use(api);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification,  { explorer: true }));
+app.use(videoApi);
 
 app.get('/', (req, res) => {
     res.send('Hello world');
